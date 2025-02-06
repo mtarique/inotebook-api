c7d7c9f029360789ccf39ed7d3b763397119e78b
mtarique <mtarique@outlook.com>
2025-02-07 00:27:13 +0530

    Use create instead of save

--- routes/auth.js
+++ routes/auth.js
@@ -19,12 +19,31 @@ router.post('/signup', [
             return res.status(400).json({ status: false, message: "Invalid user input", error: errors.array() }); 
         }
 
-        let user = new User(req.body) 
+        // // Query type 1
+        // let user = new User(req.body) 
 
-        await user.save(); 
+        // await user.save(); 
 
-        res.status(200).json({status: true, message: "New user added"})
-        
+        // res.status(200).json({status: true, message: "New user added"})
+
+        // Type 2
+        User.create({
+            name: req.body.name, 
+            email: req.body.email, 
+            password: req.body.password
+        }).then(user => res.json({status: true, message: "User is created", data: user}))
+        .catch(err => {
+            console.log(err)
+            res.json({status: false, message: "An error occurred", error: err})
+        })
+
+        // User.create(req.body)
+        // .then(user => res.json({status: true, message: "User is created", data: user}))
+        // .catch(err => {
+        //     console.log(err)
+        //     res.json({status: false, message: "An error occurred", error: err})
+        // })
+
     } catch (error) {
         console.log(error)
         res.status(500).json({status: false, message: "User could not be created", error: error})
